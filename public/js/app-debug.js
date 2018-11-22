a81ccbb98a62114ac35db57390af00015b9d1ee0
Diego Marcelo <diegomcsilva191@gmail.com>
2018-11-22 16:50:58 -0200

    Structure basic for the project

---
+++ public/js/app-debug.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var targetColor = '';
+var color = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+for (var i = 0; i < 8; i++) {
+  document.querySelector('.game').innerHTML = document.querySelector('.game').innerHTML + "<div id=\"div".concat(i, "\" ondrop=\"drop(event)\" ondragover=\"allowDrop(event)\">\n                       <p ondragstart=\"drag(event)\" draggable=\"true\" id=\"drag").concat(i, "\" class=\"div ").concat(color[Math.floor(Math.random() * 5)], "\"></p>\n                     </div>");
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+  targetColor = ev.target.parentNode.classList.value;
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  console.log(targetColor); // console.log(document.getElementById(ev.target.parentNode.id).nextSibling.previousSibling.classList.value)
+
+  console.log(document.getElementById(ev.target.parentNode.id).nextSibling.nextSibling.classList.value);
+
+  if (document.getElementById(ev.target.parentNode.id).nextSibling.nextSibling.classList.value === targetColor) {
+    alert('par');
+    document.getElementById(ev.target.parentNode.id).nextSibling.nextSibling.innerHTML = '';
+  }
+}
