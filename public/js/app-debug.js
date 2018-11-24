f28dbc8d37df21b47a469b30691c1fea78415da0
diegomcsilva <diegomcsilva191@gmail.com>
2018-11-24 18:23:42 -0200

    Dev move to vertical and horizontal directions and remove

--- public/js/app-debug.js
+++ public/js/app-debug.js
@@ -4,7 +4,7 @@ var targetColors, ev1, html;
 var colors = ['gulp', 'css', 'node', 'js', 'html'];
 
 var constructGameBlocks = function constructGameBlocks() {
-  for (var i = 1; i <= 100; i++) {
+  for (var i = 1; i <= 300; i++) {
     document.querySelector('.game').innerHTML = document.querySelector('.game').innerHTML + "<div id=\"div".concat(i, "\" ondrop=\"dragdrop.drop  (event)\" ondragover=\"dragdrop.allowDrop(event)\"><p ondragstart=\"dragdrop.drag(event)\" draggable=\"true\" id=\"drag").concat(i, "\" class=\"div ").concat(colors[Math.floor(Math.random() * 5)], "\"></p></div>");
   }
 
@@ -35,6 +35,10 @@ var dragdrop = {
       ev.target.parentNode.insertAdjacentHTML('afterend', html);
       setTimeout(function () {
         actionsGame.removeItens(document.querySelectorAll('.game .div'));
+
+        if (actionsGame.removeItens(document.querySelectorAll('.game .div'))) {
+          document.querySelector('footer').append('<div class="core"></div>');
+        }
       }, 500);
     } else if (ev1 > ev.clientX && ev1 - ev.clientX < ev.target.offsetWidth * 3) {
       html = document.getElementById(ev.dataTransfer.getData("text")).parentNode.outerHTML;
@@ -42,6 +46,10 @@ var dragdrop = {
       ev.target.parentNode.insertAdjacentHTML('beforebegin', html);
       setTimeout(function () {
         actionsGame.removeItens(document.querySelectorAll('.game .div'));
+
+        if (actionsGame.removeItens(document.querySelectorAll('.game .div'))) {
+          document.querySelector('footer').append('<div class="core"></div>');
+        }
       }, 500);
     }
   }
@@ -50,18 +58,37 @@ var actionsGame = {
   removeItens: function removeItens(list) {
     Array.from(list, function (item, index) {
       var thisColor = item.classList.value;
-      var thisColorPrev = list[index - 1] ? list[index - 1] : false;
+      var thisColorPrev = list[index - 1] && index - 1 < 72 ? list[index - 1] : false;
       var thisColorNext = list[index + 1] ? list[index + 1] : false;
+      var thisColorPrevVert = list[index - 8] && index - 8 < 72 ? list[index - 8] : false;
+      var thisColorNextVert = list[index - 8] && index - 8 < 72 ? list[index + 8] : false; // Check equals itens horizontal
 
       if (thisColor === (thisColorNext ? thisColorNext.classList.value : false) && thisColor === (thisColorPrev ? thisColorPrev.classList.value : false)) {
         item.classList.add('equals');
         thisColorPrev.classList.add('equals');
         thisColorNext.classList.add('equals');
+        console.log(item.parentNode);
         setTimeout(function () {
-          item.parentNode.parentNode.removeChild(item.parentNode);
-          thisColorPrev.parentNode.parentNode.removeChild(thisColorPrev.parentNode);
-          thisColorNext.parentNode.parentNode.removeChild(thisColorNext.parentNode);
+          item.parentNode.parentNode ? item.parentNode.parentNode.removeChild(item.parentNode) : false;
+          thisColorPrev.parentNode.parentNode ? thisColorPrev.parentNode.parentNode.removeChild(thisColorPrev.parentNode) : false;
+          thisColorNext.parentNode.parentNode ? thisColorNext.parentNode.parentNode.removeChild(thisColorNext.parentNode) : false;
+          actionsGame.removeItens(document.querySelectorAll('.game .div'));
         }, 1000);
+        return true;
+      } // Check equals itens vertical
+
+
+      if (thisColor === (thisColorNextVert ? thisColorNextVert.classList.value : false) && thisColor === (thisColorPrevVert ? thisColorPrevVert.classList.value : false)) {
+        item.classList.add('equals');
+        thisColorPrevVert.classList.add('equals');
+        thisColorNextVert.classList.add('equals');
+        setTimeout(function () {
+          item.parentNode.parentNode ? item.parentNode.parentNode.removeChild(item.parentNode) : false;
+          thisColorPrevVert.parentNode.parentNode ? thisColorPrevVert.parentNode.parentNode.removeChild(thisColorPrevVert.parentNode) : false;
+          thisColorNextVert.parentNode.parentNode ? thisColorNextVert.parentNode.parentNode.removeChild(thisColorNextVert.parentNode) : false;
+          actionsGame.removeItens(document.querySelectorAll('.game .div'));
+        }, 1000);
+        return true;
       }
     });
   }
--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ let targetColors, ev1, html;
 const colors = ['gulp', 'css', 'node', 'js', 'html'];
 
 const constructGameBlocks = () => {
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 300; i++) {
     document
       .querySelector('.game')
       .innerHTML = document.querySelector('.game').innerHTML + `<div id="div${i}" ondrop="dragdrop.drop  (event)" ondragover="dragdrop.allowDrop(event)"><p ondragstart="dragdrop.drag(event)" draggable="true" id="drag${i}" class="div ${colors[Math.floor(Math.random() * 5)]}"></p></div>`
@@ -42,6 +42,10 @@ const dragdrop = {
       ev.target.parentNode.insertAdjacentHTML('afterend', html);
       setTimeout(function() {
         actionsGame.removeItens(document.querySelectorAll('.game .div'));
+
+        if(actionsGame.removeItens(document.querySelectorAll('.game .div'))) {
+          document.querySelector('footer').append('<div class="core"></div>')
+        }
       }, 500)
     } else if ((ev1 > ev.clientX) && ((ev1 - ev.clientX) < (ev.target.offsetWidth * 3))) {
       html = document.getElementById(ev.dataTransfer.getData("text")).parentNode.outerHTML
@@ -49,8 +53,12 @@ const dragdrop = {
       ev.target.parentNode.insertAdjacentHTML('beforebegin', html);
       setTimeout(function() {
         actionsGame.removeItens(document.querySelectorAll('.game .div'));
+
+        if(actionsGame.removeItens(document.querySelectorAll('.game .div'))) {
+          document.querySelector('footer').append('<div class="core"></div>')
+        }
       }, 500)
-    }
+    } 
   }
 }
 
@@ -58,19 +66,46 @@ const actionsGame = {
   removeItens : (list) => {
     Array.from(list, (item, index) => {
       let thisColor = item.classList.value;
-      let thisColorPrev = list[index - 1] ? list[index - 1] : false;
+      let thisColorPrev = (list[index - 1] && (index - 1) < 72) ? list[index - 1] : false;
       let thisColorNext = list[index + 1] ? list[index + 1] : false;
+
+      let thisColorPrevVert = (list[index - 8] && (index - 8) < 72) ? list[index - 8] : false;
+      let thisColorNextVert = (list[index - 8] && (index - 8) < 72) ? list[index + 8] : false;
     
+      // Check equals itens horizontal
       if((thisColor === (thisColorNext ? thisColorNext.classList.value : false)) && (thisColor === (thisColorPrev ? thisColorPrev.classList.value : false ))) {
         item.classList.add('equals');
-        thisColorPrev.classList.add('equals');
+        thisColorPrev.classList.add('equals');  
         thisColorNext.classList.add('equals');
 
+        console.log(item.parentNode)
+
         setTimeout(() => {
-          item.parentNode.parentNode.removeChild(item.parentNode);
-          thisColorPrev.parentNode.parentNode.removeChild(thisColorPrev.parentNode);
-          thisColorNext.parentNode.parentNode.removeChild(thisColorNext.parentNode);
+          item.parentNode.parentNode ? item.parentNode.parentNode.removeChild(item.parentNode) : false;
+          thisColorPrev.parentNode.parentNode ? thisColorPrev.parentNode.parentNode.removeChild(thisColorPrev.parentNode) : false;
+          thisColorNext.parentNode.parentNode ? thisColorNext.parentNode.parentNode.removeChild(thisColorNext.parentNode) : false;
+
+          actionsGame.removeItens(document.querySelectorAll('.game .div'));
         }, 1000);
+
+        return true;
+      }
+
+      // Check equals itens vertical
+      if((thisColor === (thisColorNextVert ? thisColorNextVert.classList.value : false)) && (thisColor === (thisColorPrevVert ? thisColorPrevVert.classList.value : false ))) {
+        item.classList.add('equals');
+        thisColorPrevVert.classList.add('equals');
+        thisColorNextVert.classList.add('equals');
+
+        setTimeout(() => {
+          item.parentNode.parentNode ? item.parentNode.parentNode.removeChild(item.parentNode) : false;
+          thisColorPrevVert.parentNode.parentNode ? thisColorPrevVert.parentNode.parentNode.removeChild(thisColorPrevVert.parentNode) : false;
+          thisColorNextVert.parentNode.parentNode ? thisColorNextVert.parentNode.parentNode.removeChild(thisColorNextVert.parentNode) : false;
+
+          actionsGame.removeItens(document.querySelectorAll('.game .div'));
+        }, 1000);
+
+        return true;
       }
     })
   }
